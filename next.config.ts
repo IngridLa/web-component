26f516e4de2b6a05929f80ac136edec6beee9c64
Ingrid Larissa <Larissaorcx@gmail.com>
2025-04-05 14:27:41 -0300

    initial config host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// remote/next.config.js 
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {  
+  reactStrictMode: true,
+  webpack(config:any, options:any) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "web-component",
+        remotes: {
+          "test-web-componen": `test-web-componen@http://localhost:3001/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./Button": "./src/components/Button.tsx",
+        },
+        shared: {},
+      })
+    );
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+export default function Button() {
+  return (
+    <button type="button" className="bg-blue-500 text-white py-2 px-4 rounded w-[200px] h-[50px] cursor-pointer hover:bg-blue-700 transition duration-300 ease-in-out">
+      Click Me
+    </button>
+  );
+}
+
